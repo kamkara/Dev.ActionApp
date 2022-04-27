b037abac37e30a8bb2c33910edc80e385fe5153e
kamkara <lnclass.kamkara@gmail.com>
2022-04-27 15:17:06 +0000

    add abouts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  get "nous-decouvrir", to:"abouts#index"
   root to:'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
