6b5848182ff22363d351c94c2c348a0c09b7e320
kamkara <lnclass.kamkara@gmail.com>
2022-04-27 15:22:57 +0000

    add involved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  
+
   get "nous-decouvrir", to:"abouts#index"
+  get "agisons-ensemble", to:"involved#index"
   root to:'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
